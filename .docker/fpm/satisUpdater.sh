e494a06027696d48adbb637fb0108f6668826593
Damien Sauvée <damien.sauvee@recommerce.com>
2024-02-19 14:50:02 +0100

    fix: Fix path where Satis is build

--- .docker/fpm/satisUpdater.sh
+++ .docker/fpm/satisUpdater.sh
@@ -2,8 +2,7 @@
 
 
 while true; do
-        php /var/www/satis/bin/satis build /var/www/satis/satis.json /var/www/satis/web/ -vvv
+        php /var/www/satis/bin/satis build /var/www/satis/satis.json /var/www/public/ -vvv
 	echo "done"
         sleep 180
 done
-
